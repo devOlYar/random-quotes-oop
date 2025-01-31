24e64a234f3f208a502dc23da03e3f165ad68fde
Oleksandr Yaroshenko <oleksandr.yaroshenko.developer@gmail.com>
2025-01-31 19:28:57 +0200

    Initial version of the quotes app using OOP and classes - chosing random quote from the array of quotes

---
+++ src/data/quotes.js
@@ -0,0 +1,59 @@
+const quotes = [
+  {
+    id: 'a1b2c3d4',
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 'e5f6g7h8',
+    text: 'Innovation distinguishes between a leader and a follower',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 'i9j0k1l2',
+    text: "Your time is limited, so don't waste it living someone else's life",
+    author: 'Steve Jobs',
+  },
+  {
+    id: 'm3n4o5p6',
+    text: 'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful',
+    author: 'Albert Schweitzer',
+  },
+  {
+    id: 'q7r8s9t0',
+    text: 'The best way to predict the future is to invent it',
+    author: 'Alan Kay',
+  },
+  {
+    id: 'u1v2w3x4',
+    text: 'Life is 10% what happens to us and 90% how we react to it',
+    author: 'Charles R. Swindoll',
+  },
+  {
+    id: 'y5z6a7b8',
+    text: 'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment',
+    author: 'Buddha',
+  },
+  {
+    id: 'c9d0e1f2',
+    text: 'The purpose of our lives is to be happy',
+    author: 'Dalai Lama',
+  },
+  {
+    id: 'g3h4i5j6',
+    text: 'You miss 100% of the shots you don’t take',
+    author: 'Wayne Gretzky',
+  },
+  {
+    id: 'k7l8m9n0',
+    text: 'In the middle of every difficulty lies opportunity',
+    author: 'Albert Einstein',
+  },
+  {
+    id: 'o1p2q3r4',
+    text: 'What lies behind us and what lies before us are tiny matters compared to what lies within us',
+    author: 'Ralph Waldo Emerson',
+  },
+];
+
+export default quotes;

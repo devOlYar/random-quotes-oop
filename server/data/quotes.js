a1fc3e7691f072ae398a42a702ea897497a5d9e2
Oleksandr Yaroshenko <oleksandr.yaroshenko.developer@gmail.com>
2025-02-05 21:19:05 +0200

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,64 @@
+const quotes = [
+  {
+    id: 'z9y8x7w6',
+    text: 'Dream big and dare to fail.',
+    author: 'Norman Vaughan',
+  },
+  {
+    id: 'v5u4t3s2',
+    text: 'The only limit to our realization of tomorrow is our doubts of today.',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    id: 'r1q2p3o4',
+    text: 'Act as if what you do makes a difference. It does.',
+    author: 'William James',
+  },
+  {
+    id: 'n5m6l7k8',
+    text: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+    author: 'Winston Churchill',
+  },
+  {
+    id: 'j9i8h7g6',
+    text: 'Happiness depends upon ourselves.',
+    author: 'Aristotle',
+  },
+  {
+    id: 'f5e4d3c2',
+    text: 'Life is really simple, but we insist on making it complicated.',
+    author: 'Confucius',
+  },
+  {
+    id: 'b1a2z3y4',
+    text: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    author: 'Nelson Mandela',
+  },
+  {
+    id: 'x5w6v7u8',
+    text: 'It always seems impossible until it’s done.',
+    author: 'Nelson Mandela',
+  },
+  {
+    id: 't9s8r7q6',
+    text: 'If you look at what you have in life, you’ll always have more.',
+    author: 'Oprah Winfrey',
+  },
+  {
+    id: 'p5o4n3m2',
+    text: 'Do what you can, with what you have, where you are.',
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: 'l1k2j3i4',
+    text: 'Keep your face always toward the sunshine—and shadows will fall behind you.',
+    author: 'Walt Whitman',
+  },
+  {
+    id: 'h5g6f7e8',
+    text: 'Success usually comes to those who are too busy to be looking for it.',
+    author: 'Henry David Thoreau',
+  },
+];
+
+module.exports = quotes;

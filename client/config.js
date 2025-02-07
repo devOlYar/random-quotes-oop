08be3d06a18d72ea23ff1b45cf789a4395edd066
Oleksandr Yaroshenko <oleksandr.yaroshenko.developer@gmail.com>
2025-02-07 19:43:48 +0200

    Change API_URL to my public API URL

--- client/config.js
+++ client/config.js
@@ -1,6 +1,6 @@
 const config = {
   PUBLIC_API_URL: 'https://quoteslate.vercel.app/api',
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://random-quotes-oop-8jy3.onrender.com',
 };
 
 export default config;
